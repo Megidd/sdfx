b8e6512489160ef6165e9f82e80cd884e57214f9
Megidd Git <megiddgit@gmail.com>
2023-01-09 14:22:32 +0330

    Play around with text example: thickness, speed...

--- examples/text/main.go
+++ examples/text/main.go
@@ -27,10 +27,10 @@ func main() {
 		log.Fatalf("can't read font file %s\n", err)
 	}
 
-	t := sdf.NewText("SDFX!\nHello,\nWorld!")
+	t := sdf.NewText("Text.\nCoverted to mesh.\nBy Go.")
 	//t := sdf.NewText("相同的不同")
 
-	s2d, err := sdf.TextSDF2(f, t, 10.0)
+	s2d, err := sdf.TextSDF2(f, t, 20.0)
 	if err != nil {
 		log.Fatalf("can't generate text sdf2 %s\n", err)
 	}
@@ -38,11 +38,11 @@ func main() {
 	render.ToDXF(s2d, "shape.dxf", render.NewMarchingSquaresQuadtree(600))
 	render.ToSVG(s2d, "shape.svg", render.NewMarchingSquaresQuadtree(600))
 
-	s3d, err := sdf.ExtrudeRounded3D(s2d, 1.0, 0.2)
+	s3d, err := sdf.ExtrudeRounded3D(s2d, 8.0, 0.2)
 	if err != nil {
 		log.Fatal(err)
 	}
-	render.ToSTL(s3d, "shape.stl", render.NewMarchingCubesOctree(600))
+	render.ToSTL(s3d, "shape.stl", render.NewMarchingCubesUniform(200))
 }
 
 //-----------------------------------------------------------------------------
